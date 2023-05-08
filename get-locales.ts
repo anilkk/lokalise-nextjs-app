12c07f6192ca5d80b2bdd1c8b2ee91ac8ae9387a
Anil Kumar <anil.kumar@lokalise.com>
2023-05-08 20:38:59 +0200

    Add locales changes

---
+++ get-locales.ts
@@ -0,0 +1,11 @@
+import 'server-only'
+import type { Locale } from './i18n-config'
+
+// We enumerate all locales here for better linting and typescript support
+// We also get the default import for cleaner types
+const locales = {
+  en: () => import('./locales/en.json').then((module) => module.default),
+  de: () => import('./locales/de.json').then((module) => module.default),
+}
+
+export const getDictionary = async (locale: Locale) => locales[locale]()
