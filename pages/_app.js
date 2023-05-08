12092bc1a3a9f0c9a085bda16cf4be13bef011e0
Anil Kumar <anil.kumar@lokalise.com>
2023-05-08 17:10:30 +0200

    init Animate On Scroll Library

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
+import { useEffect } from 'react'
+import AOS from 'aos'
+
 import '../css/styles.css'
+import 'aos/dist/aos.css'
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: 'phone',
+      duration: 700,
+      easing: 'ease-out-cubic',
+    })
+  })
   return <Component {...pageProps} />
 }
