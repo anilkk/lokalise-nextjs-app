a67129951c36b0115374869cafa8d78faecb623e
Anil Kumar <anil.kumar@lokalise.com>
2023-05-08 15:29:51 +0200

    tailwind addition

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,21 +9,38 @@ export default function IndexPage() {
 
   return (
     <div>
-      <h1>Index page</h1>
-      <p>Current locale: {locale}</p>
-      <p>Default locale: {defaultLocale}</p>
-      <p>Configured locales: {JSON.stringify(locales)}</p>
+      <section className="relative">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
 
-      <LocaleSwitcher />
+          {/* Hero content */}
+          <div className="pt-32 pb-12 md:pt-40 md:pb-20">
 
-      <Link href="/gsp">To getStaticProps page</Link>
-      <br />
+            {/* Section header */}
+            <div className="text-center pb-12 md:pb-16">
+              <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4 text-slate-500" data-aos="zoom-y-out">Make your website <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">wonderful</span></h1>
+              <div className="max-w-3xl mx-auto">
+                <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
+              </div>
+            </div>
+          </div>
 
-      <Link href="/gsp/first">To dynamic getStaticProps page</Link>
-      <br />
+        </div>
+        <h1>Index page</h1>
+        <p>Current locale: {locale}</p>
+        <p>Default locale: {defaultLocale}</p>
+        <p>Configured locales: {JSON.stringify(locales)}</p>
 
-      <Link href="/gssp">To getServerSideProps page</Link>
-      <br />
+        <LocaleSwitcher />
+
+        <Link href="/gsp">To getStaticProps page</Link>
+        <br />
+
+        <Link href="/gsp/first">To dynamic getStaticProps page</Link>
+        <br />
+
+        <Link href="/gssp">To getServerSideProps page</Link>
+        <br />
+      </section>
     </div>
   )
 }
