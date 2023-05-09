f99230cf6f8942434eff50ee79ad2f428a9b1d34
Anil Kumar <anil.kumar@lokalise.com>
2023-05-09 17:34:38 +0200

    i18next working code

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -3,16 +3,23 @@ import { Locale } from '../../i18n-config'
 import Counter from './components/counter'
 import LocaleSwitcher from './components/locale-switcher'
 import Hero from './components/hero'
+import { useTranslation } from '../i18n'
 
 export default async function IndexPage({
   params: { lang },
 }: {
   params: { lang: Locale }
 }) {
+  const { t } = await useTranslation(lang)
   const dictionary = await getDictionary(lang)
 
   return (
     <div>
+       <h1>
+       {/*
+      // @ts-ignore */}
+        {t('title')}
+        </h1>
       <Hero />
       <LocaleSwitcher />
       <p>Current locale: {lang}</p>
