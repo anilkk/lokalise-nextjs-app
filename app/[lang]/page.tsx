8d500371eb5dd92352e5d464f0f039a2f240d2bb
Anil Kumar <anil.kumar@lokalise.com>
2023-05-15 06:39:17 +0200

    Remove unsed code

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -24,11 +24,6 @@ export default async function IndexPage({
       // @ts-ignore */}
       <Hero  lang={lang} />
       <p>Current locale: {lang}</p>
-      {/* <p>
-        This text is rendered on the server:{' '}
-        {dictionary['server-component'].welcome}
-      </p>
-      <Counter dictionary={dictionary.counter} /> */}
       {/*
       // @ts-ignore */}
       <Features lang={lang}/>
