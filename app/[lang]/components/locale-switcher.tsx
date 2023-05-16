5c3ff40f1b888ff479d49a400865e1f4fdc2aebd
Anil Kumar <anil.kumar@lokalise.com>
2023-05-16 18:01:01 +0200

    bring back the basic language switcher

--- app/[lang]/components/locale-switcher.tsx
+++ app/[lang]/components/locale-switcher.tsx
@@ -1,11 +1,12 @@
 'use client'
-
+import React, { Fragment } from 'react';
 import { usePathname } from 'next/navigation'
-import { Menu } from '@headlessui/react'
+import { Menu, Transition } from '@headlessui/react'
 import Link from 'next/link'
 import { i18n } from '../../../i18n-config'
 
 export default function LocaleSwitcher() {
+  const [selected, setSelected] = React.useState(0);
   const pathName = usePathname()
   const redirectedPathName = (locale: string) => {
     if (!pathName) return '/'
@@ -26,17 +27,36 @@ export default function LocaleSwitcher() {
           )
         })}
       </ul>
-      <Menu>
-        <Menu.Button>Options</Menu.Button>
-        <Menu.Items>
-          {i18n.locales.map((locale) => {
-            /* Use the `active` state to conditionally style the active item. */
-            return (<Menu.Item key={redirectedPathName(locale)}>
-                 <Link href={redirectedPathName(locale)}>{locale}</Link>
-            </Menu.Item>)
-            })}
-        </Menu.Items>
-      </Menu>
+      {/* <Menu as="div" className="relative inline-block text-left">
+      {({ open }) => (
+        <>
+        <div>
+          <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              Options
+          </Menu.Button>
+        </div>
+        <Transition
+          show={open}
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items
+              className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"> 
+            {i18n.locales.map((locale, index) => {
+              return (<Menu.Item key={redirectedPathName(locale)}>
+                  <Link href={redirectedPathName(locale)} className={`text-gray-900 block px-4 py-2 text-sm fixed top-0 ${index? 'right-8' :'right-0'}`}>{locale}</Link>
+              </Menu.Item>)
+              })}
+          </Menu.Items>
+        </Transition>
+        </>
+      )}
+      </Menu> */}
     </div>
   )
 }
